884359098b1b2bfa73346d11e4e89552abd594cc
catitgirl <natagan888@gmail.com>
2023-01-24 17:43:33 +0400

    add json

--- number1/script.js
+++ number1/script.js
@@ -1,53 +1,37 @@
-const header = document.querySelector('header');
-const section = document.querySelector('section');
-const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-const request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-   const superHeroes = request.response;
-    populateHeader(superHeroes);
-    showHeroes(superHeroes);
-  }
-  function populateHeader(jsonObj) {
-    const myH1 = document.createElement('h1');
-    myH1.textContent = jsonObj['squadName'];
-    header.appendChild(myH1);
-  
-    const myPara = document.createElement('p');
-    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-    header.appendChild(myPara);
-  }
-  function showHeroes(jsonObj) {
-    const heroes = jsonObj['members'];
-  
-    for (let i = 0; i < heroes.length; i++) {
-        const myArticle = document.createElement('article');
-        const myH2 = document.createElement('h2');
-        const myPara1 = document.createElement('p');
-        const myPara2 = document.createElement('p');
-        const myPara3 = document.createElement('p');
-        const myList = document.createElement('ul');
-  
-      myH2.textContent = heroes[i].name;
-      myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-      myPara2.textContent = 'Age: ' + heroes[i].age;
-      myPara3.textContent = 'Superpowers:';
-  
-      const superPowers = heroes[i].powers;
-      for (var j = 0; j < superPowers.length; j++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = superPowers[j];
-        myList.appendChild(listItem);
-      }
-  
-      myArticle.appendChild(myH2);
-      myArticle.appendChild(myPara1);
-      myArticle.appendChild(myPara2);
-      myArticle.appendChild(myPara3);
-      myArticle.appendChild(myList);
-  
-      section.appendChild(myArticle);
+const json = ` [{
+        "name" : "Molecule Man",
+        "age" : 29,
+        "secretIdentity" : "Dan Jukes",
+        "powers": "laserblast"
+      },
+      {
+        "name" : "Madame Uppercut",
+        "age" : 39,
+        "secretIdentity" : "Jane Wilson",
+        "powers" : "can fire"
+      },
+      {
+        "name" : "Eternal Flame",
+        "age" : 1000000,
+        "secretIdentity" : "Unknown",
+        "powers" : "can fire"
     }
-  }
+]`;
+  debugger
+
+  document.addEventListener("DOMContentLoaded", function
+  (event) {
+    let heroes = JSON.parse(json);
+
+    let heroesContent = "";
+    for (let hero of heroes) {
+        heroesContent += `<div class='hero'>
+        <h2> ${hero.name}</h2>
+        <div>Age: ${hero.age}</div>
+        <div>${hero.secretIdentity}</div>
+        <div>Power: ${hero.powers}</div>
+        </div>`;
+    }
+
+    document.getElementById("heroesContainer").innerHTML = heroesContent;
+  });
