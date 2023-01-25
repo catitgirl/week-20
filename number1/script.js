3de2a3e69f1834b14519a99048ed6ff8ce5fc03f
catitgirl <natagan888@gmail.com>
2023-01-25 19:38:52 +0400

    add js

--- number1/script.js
+++ number1/script.js
@@ -18,16 +18,16 @@ const json = ` [{
         "marital status" : "daughter of the Red Guard",
         "occupation" : "Spy, assassin, later superheroine"  
       },
-      {
-        "name" : "Power Woman",
-        "position" : "good",
-        "psevdonims" : "Jewel, Knightress, Power Woman",
-        "species" : "mutant",
-        "height": "163 cm",
-        "weight" : "54 kg",
-        "marital" : "Married to Luke Cage",
-        "occupation" : "Superhero, private detective, journalist" 
-    }
+    {
+      "name" : "Wonder Woman",
+      "position" : "good",
+      "psevdonims" : "Diana Prince",
+      "species" : "mutant",
+      "height": "183 cm",
+      "weight" : "75 kg",
+      "marital" : "Single",
+      "occupation" : "Superhero, assistant secretary" 
+  }
 ]`;
 
   document.addEventListener("DOMContentLoaded", function
@@ -38,15 +38,77 @@ const json = ` [{
     for (let hero of heroes) {
         heroesContent += `<div class='hero'>
         <h2> ${hero.name}</h2>
-        <div><p>Position:</p> ${hero.position}</div>
-        <div><p>Psevdonims:</p>${hero.psevdonims}</div>
-        <div><p>Species:</p> ${hero.species}</div>
-        <div><p>Height:</p> ${hero.height}</div>
-        <div><p>Weight:</p> ${hero.weight}</div>
-        <div><p>Marital status:</p> ${hero.marital}</div>
-        <div><p>Occupation:</p> ${hero.occupation}</div>
+        <div><strong>Position:</strong> ${hero.position}</div>
+        <div><strong>Psevdonims:</strong>${hero.psevdonims}</div>
+        <div><strong>Species:</strong> ${hero.species}</div>
+        <div><strong>Height:</strong> ${hero.height}</div>
+        <div><strong>Weight:</strong> ${hero.weight}</div>
+        <div><strong>Marital status:</strong> ${hero.marital}</div>
+        <div><strong>Occupation:</strong> ${hero.occupation}</div>
         </div>`;
     }
 
     document.getElementById("heroesContainer").innerHTML = heroesContent;
   });
+
+
+// rating
+
+"use strict"
+const ratings = document.querySelectorAll('.rating');
+if (ratings.length > 0) {
+  initRatings();
+}
+
+function initRatings() {
+  let ratingActive, ratingValue;
+  for (let index = 0; index < ratings.length; index++) {
+    const rating = ratings[index];
+    initRating(rating);
+  }
+
+  function initRating(rating) {
+iniRatingsVars(rating);
+
+setRatingActiveWidth();
+
+if (rating.classList.contains('rating_set')) {
+  setRating(rating);
+}
+  }
+
+  function iniRatingsVars(rating) {
+    ratingActive = rating.querySelector('.rating__active');
+    ratingValue = rating.querySelector('.rating__value');
+  }
+
+  function setRatingActiveWidth(index = ratingValue.innerHTML) {
+    const ratingActiveWidth = index / 0.05;
+    ratingActive.style.width = `${ratingActiveWidth}%`;
+  }
+
+  function setRating(rating) {
+const ratingItems = rating.querySelectorAll('.rating__item');
+for (let index = 0; index < ratingItems.length; index++) {
+  const ratingItem = ratingItems[index];
+  ratingItem.addEventListener("mouseenter", function (e) {
+    iniRatingsVars(rating);
+    setRatingActiveWidth(ratingItem.value);
+  });
+  ratingItem.addEventListener("mouseleave", function (e) {
+    setRatingActiveWidth();
+});
+
+ratingItem.addEventListener("click", function (e) {
+iniRatingsVars(rating);
+
+if (rating.dataset.ajax) {
+  setRatingValue(ratingItem.value, rating);
+} else {
+ratingValue.innerHTML = index +1;
+setRatingActiveWidth();
+}
+});
+  }
+}
+}
